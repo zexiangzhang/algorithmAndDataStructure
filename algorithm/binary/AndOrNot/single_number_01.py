45eb8bba418072b78aa87dbfa14ed490ffc7a2ab
zexinag <zexiangzhang@163.com>
2021-01-19 18:53:07 +0800

    二进制相关

---
+++ algorithm/binary/AndOrNot/single_number_01.py
@@ -0,0 +1,22 @@
+"""
+ 给定一个非空整数数组，除了某个元素只出现一次以外，其余每个元素均出现两次。找出那个只出现了一次的元素
+ example 1:
+    输入: [2,2,1]
+    输出: 1
+ example 2:
+    输入: [4,1,2,1,2]
+    输出: 4
+"""
+from typing import List
+
+
+def single_number(numbers: List[int]) -> int:
+    result = 0
+    for number in numbers:
+        result ^= number
+    return result
+
+
+if __name__ == '__main__':
+    print(single_number([2, 2, 1]))
+    print(single_number([4, 1, 2, 1, 2]))
---
+++ algorithm/binary/AndOrNot/single_number_02.py
@@ -0,0 +1,23 @@
+"""
+ 给定一个非空整数数组，除了某个元素只出现一次以外，其余每个元素均出现了三次。找出那个只出现了一次的元素
+ example 1:
+    输入: [2,2,3,2]
+    输出: 3
+ example 2:
+    输入: [0,1,0,1,0,1,99]
+    输出: 99
+"""
+from typing import List
+
+
+def single_number(numbers: List[int]) -> int:
+    pass
+
+
+if __name__ == '__main__':
+    # 6 / 2 = 3
+    # 3 / 2 = 1 余 1
+    # 6 ：
+    print(6 & 11)
+    # print(single_number([2, 2, 3, 2]))
+    # print(single_number([0, 1, 0, 1, 0, 1, 99]))
